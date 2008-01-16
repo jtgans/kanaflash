fab2c9ad8c0cc633fc33b2c929c763f85bbefbae
June Tate-Gans <june@theonelab.com>
2008-01-16 03:08:20 -0700

    Initial commit of a mostly working verision of kanaflash.

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  kanaflash
+//
+//  Created by June Tate-Gans on 1/16/08.
+//  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
---
+++ rb_main.rb
@@ -0,0 +1,26 @@
+#
+#  rb_main.rb
+#  kanaflash
+#
+#  Created by June Tate-Gans on 1/16/08.
+#  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+#
+
+require 'osx/cocoa'
+include OSX
+
+$KCODE="utf8"
+
+def rb_main_init
+  path = OSX::NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+  rbfiles = Dir.entries(path).select {|x| /\.rb\z/ =~ x}
+  rbfiles -= [ File.basename(__FILE__) ]
+  rbfiles.each do |path|
+    require( File.basename(path) )
+  end
+end
+
+if $0 == __FILE__ then
+  rb_main_init
+  OSX.NSApplicationMain(0, nil)
+end
